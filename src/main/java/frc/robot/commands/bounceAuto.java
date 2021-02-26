713008e4164b867f1b25d6977a7fbb07f15e83a3
bkepps <benskepp@gmail.com>
2021-02-26 13:58:28 -0600

    imports pathweaver json's to trajectory

--- src/main/java/frc/robot/commands/bounceAuto.java
+++ src/main/java/frc/robot/commands/bounceAuto.java
@@ -5,19 +5,27 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.MyDriveTrain;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.DriverStation;             //for error reporting
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.Filesystem;
+
+import java.nio.file.Path;
+import java.io.IOException;
 
 /** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
+public class bounceAuto extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+  private final MyDriveTrain m_subsystem;
 
   /**
-   * Creates a new ExampleCommand.
+   * Creates a new bounceAuto.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
+  public bounceAuto(MyDriveTrain subsystem) {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
@@ -25,7 +33,16 @@ public class ExampleCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    String trajectoryJSON = "paths/YourPath.wpilib.json";
+    Trajectory trajectory = new Trajectory();
+    try {
+      Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+      trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+    } catch (IOException ex) {
+      DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+    }
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
