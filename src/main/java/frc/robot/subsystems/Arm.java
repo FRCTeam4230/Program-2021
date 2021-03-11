b0f9acc1a7a224bb4c052f35ed13d7f9393f79de
bkepps <benskepp@gmail.com>
2021-03-11 13:27:49 -0600

    added arm subsystem frgot to add arm.java last time

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Arm extends SubsystemBase {
+  /**
+   * Creates a new ExampleSubsystem.
+   */
+  private final TalonSRX arm;
+  
+
+  public Arm() {
+
+    arm = new TalonSRX(Constants.CANId.kArm);
+
+  }
+
+  public void armUp(){
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run. why? idk
+  }
+}
